c31de8544793b169456a09f1c3713a85338704dc
NeptuneRjo <rjo9000@outlook.com>
2022-01-02 00:30:42 -0500

    setup

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { ContextExclusionPlugin } from 'webpack';
+import './style.css';
+
+// function content() {
+//     const header = document.createElement('div')
+//     header.id = "header";
+//     header.innerText = "Hello World"
+
+//     return header;
+// }
+
+// document.body.appendChild(content());
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
+    mode: 'development',
     entry: './src/index.js',
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist')
-    }
-}
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    resolve: {
+        fallback: {
+            util: require.resolve('util/'),
+        },
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+    ],
+};
